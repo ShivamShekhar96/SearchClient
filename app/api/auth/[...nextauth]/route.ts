7defbd16ab985d467b4e9d71f50890902b2ae1f7
Shivam Shekhar Singh <kookymonk@Shivams-MacBook-Pro.local>
2024-01-28 01:53:55 +0530

    add secret

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,6 +9,7 @@ const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  secret: process.env.NEXT_PUBLIC_SECRET,
   session: {
     strategy: "jwt",
   },
